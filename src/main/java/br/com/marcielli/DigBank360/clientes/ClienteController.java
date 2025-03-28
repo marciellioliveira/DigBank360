69268db5bdf58c4f38252e3c46defec16b002b5b
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-03-28 09:10:14 -0300

    Inicio CRUD

--- src/main/java/br/com/marcielli/DigBank360/clientes/ClienteController.java
+++ src/main/java/br/com/marcielli/DigBank360/clientes/ClienteController.java
@@ -1,9 +1,14 @@
 package br.com.marcielli.DigBank360.clientes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api")
@@ -12,9 +17,42 @@ public class ClienteController { //Controlador entre o BD (Repository) e o Usuá
 	@Autowired
 	private ClienteService clienteService;
 	
+	//GET Rota Inicial
 	@GetMapping("/") //http://localhost:8085/api/
 	public String inicio() {
 		return "Rest API de Banco Digital com Spring Boot JPA, H2 database, lombok e Padrão de Projeto de Separação em Camadas";
+	}	
+	
+	//GET - Listar
+	@GetMapping
+	public List<Cliente> getAllClients(){
+		return clienteService.getAll();
 	}
+	
+	//POST
+	@PostMapping
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	//DELETE
+	
+	
+	
+	
+	
+//private String nome;
+	
+//	private Long cpf;
+//	
+//	private LocalDate dataNascimento;
+//	
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "tb_endereco", referencedColumnName = "id")
+//	private Endereco endereco;	
+	
+	
+	
+	
 	
 }
--- src/main/java/br/com/marcielli/DigBank360/clientes/ClienteService.java
+++ src/main/java/br/com/marcielli/DigBank360/clientes/ClienteService.java
@@ -1,5 +1,7 @@
 package br.com.marcielli.DigBank360.clientes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +10,28 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	
+	//LISTAR
+	public List<Cliente> getAll(){
+		return clienteRepository.findAll();
+	}
+	
+	//CRIAR
+	public Cliente save(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	//DELETAR
+//	public void delete(Long id) {
+//		return clienteRepository.deleteById(id);
+//	}
+//	
+	
+	
+	
+	
+	
+	
 
 }
